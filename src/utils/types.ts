da02ef5d3afdd0dda351c84004c280901276d145
Bieniu <mikibien@outlook.com>
2022-06-05 20:31:17 +0200

    feat: add types

---
+++ src/utils/types.ts
@@ -0,0 +1,16 @@
+
+export type CanvasObject ={
+    array: number[],
+    canvas: HTMLCanvasElement,
+    ctx: CanvasRenderingContext2D,
+}
+
+export type DrawZoneProperty = {
+    istart: number,
+    iend: number,
+}
+
+export type ColorProperty = {
+    bgColor: string,
+    columnColor: string,
+}

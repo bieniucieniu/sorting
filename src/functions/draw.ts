7b42bd3baf5ec87697a4512eedf37d87ec96fa5c
Bieniu <mikibien@outlook.com>
2022-06-08 07:36:46 +0200

    fix: :art: set default draw func background color to transparent

--- src/functions/draw.ts
+++ src/functions/draw.ts
@@ -8,17 +8,18 @@ export const draw = (canvasObject: CanvasObject, drawZoneProperty?: DrawZoneProp
         iend: array.length,
         };
     const { bgColor, columnColor } = colorProperty || {
-        bgColor: '#fff',
-        columnColor: '#000',
+        bgColor: '#ffffff00',
+        columnColor: '#000000',
         };
+
+    let barWidth = canvas.width / array.length;
+    let barHeight = canvas.height / array.length;
 
     ctx.fillStyle = bgColor;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     ctx.fillStyle = columnColor;
-    for (let i = istart; i < iend; i++) {
-        const x = i * 10;
-        const y = array[i] * 10;
-        ctx.fillRect(x, y, 10, 10);
+    for(let i = istart; i <= iend; i++) {
+        ctx.fillRect(barWidth * i, canvas.height - array[i] * barHeight, barWidth, array[i] * barHeight);
     }
 }

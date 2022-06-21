614557d1041249af4438307e5aaf8bf89c5a7b4d
Bieniu <mikibien@outlook.com>
2022-06-21 09:12:00 +0200

    feat: :sparkles: making button respoding

--- src/main.ts
+++ src/main.ts
@@ -1,38 +1,47 @@
-import { drawArray , sortingAnimation, stopAnimation, calculateSteps } from "./functions";
-import {range, shuffle, sleep} from "./utils";
+import {
+    drawArray,
+    sortingAnimation,
+    stopAnimation,
+    calculateSteps,
+} from "./functions";
+import { CurrentAlgorithm, range, shuffle, sleep } from "./utils";
 
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
-const bodyStyles = window.getComputedStyle(document.querySelector(".main-content") as HTMLElement ,null) as CSSStyleDeclaration;
+const bodyStyles = window.getComputedStyle(
+    document.querySelector(".main-content") as HTMLElement,
+    null
+) as CSSStyleDeclaration;
+
+const currentAlgorithm: CurrentAlgorithm = {
+    name: "bubbleSort",
+    document: document.querySelector(".current-algorithm-name") as HTMLElement,
+};
 
 canvas.width = canvas.parentElement?.clientWidth || 300;
 canvas.height = canvas.parentElement?.clientHeight || 300;
 
 let array = shuffle(range(30));
 
-
 let canvasObj = {
     canvas,
     ctx,
-}
+};
 
-let arrayObj = {
-    array: [...array],
-    steps: calculateSteps(array),
-}
+let arrayObj = calculateSteps(array, currentAlgorithm.name);
 
 let stylingProp = {
-    bgColor: bodyStyles.getPropertyValue('background-color'),
-    fgColor: '#000000',
+    bgColor: bodyStyles.getPropertyValue("background-color"),
+    fgColor: "#000000",
     gap: 5,
-    rise: 10
-}
+    rise: 10,
+};
 
 let animationProp = {
-    delay: 400,
+    delay: 4,
     swapColor: "red",
-    compareColor: "green"
-} 
+    compareColor: "green",
+};
 
 drawArray(canvasObj, arrayObj, stylingProp);
 
@@ -43,55 +52,62 @@ window.addEventListener("resize", () => {
     drawArray(canvasObj, arrayObj, stylingProp);
 });
 
+const reset = async () => {
+    stopAnimation();
+    array = shuffle(range(30));
+    arrayObj = calculateSteps(array, currentAlgorithm.name);
+    await sleep(animationProp.delay);
+    drawArray(canvasObj, arrayObj, stylingProp);
+};
 
-let currentAlgorithmName: string;
 
-document.querySelector
 
-document.querySelectorAll(".btn").forEach(btn => {
-
+document.querySelectorAll(".btn").forEach((btn) => {
     switch (btn.id) {
         case "bubble-sort":
             btn.addEventListener("click", () => {
-            currentAlgorithmName = "bubbleSort";
-        });
-        break;
+                currentAlgorithm.name = "BubbleSort";
+                currentAlgorithm.document.innerHTML = currentAlgorithm.name;
+                reset();
+            });
+            break;
         case "insertion-sort":
             btn.addEventListener("click", () => {
-            currentAlgorithmName = "insertionSort";
-        });
-        break;
+                currentAlgorithm.name = "InsertionSort";
+                currentAlgorithm.document.innerHTML = currentAlgorithm.name;
+                reset();
+            });
+            break;
         case "merge-sort":
             btn.addEventListener("click", () => {
-            currentAlgorithmName = "mergeSort";
-        });
-        break;
+                currentAlgorithm.name = "MergeSort";
+                currentAlgorithm.document.innerHTML = currentAlgorithm.name;
+                reset();
+            });
+            break;
         case "quick-sort":
             btn.addEventListener("click", () => {
-            currentAlgorithmName = "quickSort";
-        });
-        break;
+                currentAlgorithm.name = "QuickSort";
+                currentAlgorithm.document.innerHTML = currentAlgorithm.name;
+                reset();
+            });
+            break;
         case "reset":
             btn.addEventListener("click", async () => {
-                console.log(currentAlgorithmName);
-
-                stopAnimation();
-                array = shuffle(range(30));
-                arrayObj = {
-                    array: [...array],
-                    steps: calculateSteps(array, currentAlgorithmName),
-                }
-                await sleep(animationProp.delay);
-                drawArray(canvasObj, arrayObj, stylingProp);
+                reset();
             });
             break;
         case "start":
-            btn.addEventListener("click",  () => {
-                sortingAnimation(canvasObj, arrayObj, stylingProp, animationProp);
+            btn.addEventListener("click", () => {
+                sortingAnimation(
+                    canvasObj,
+                    arrayObj,
+                    stylingProp,
+                    animationProp
+                );
             });
             break;
         default:
             break;
     }
-            
 });
